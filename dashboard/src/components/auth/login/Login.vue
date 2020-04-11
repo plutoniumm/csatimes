af146905864bd37a12e52ea738b2c7614af22a26
Aviral09 <aviralgoel2001@gmail.com>
2020-04-11 21:26:42 +0530

    Implemented GoogleSignIn and Notice board
    Googe sign in has been implemented.(Refresh the login page until the button appears)
    Notice board with collapsible notices supporting upto 5 latest updates has been implemented on the dashboard.

--- dashboard/src/components/auth/login/Login.vue
+++ dashboard/src/components/auth/login/Login.vue
@@ -1,9 +1,9 @@
 <template>
-  <form @submit.prevent="onsubmit">
+  <!--<form @submit.prevent="oauthSignIn">
     <va-input
       v-model="email"
       type="email"
-      :label="$t('auth.email')"
+      :label="('EMAIL')"
       :error="!!emailErrors.length"
       :error-messages="emailErrors"
     />
@@ -11,49 +11,73 @@
     <va-input
       v-model="password"
       type="password"
-      :label="$t('auth.password')"
+      :label="('Password')"
       :error="!!passwordErrors.length"
       :error-messages="passwordErrors"
     />
-
-    <div class="auth-layout__options d-flex align--center justify--space-between">
-      <va-checkbox v-model="keepLoggedIn" class="mb-0" :label="$t('auth.keep_logged_in')"/>
-      <router-link class="ml-1 link" :to="{name: 'recover-password'}">{{$t('auth.recover_password')}}</router-link>
+    <div class="d-flex justify -center mt-3">
+      <va-button type="submit">Login</va-button>
     </div>
-
-    <div class="d-flex justify--center mt-3">
-      <va-button type="submit" class="my-0">{{ $t('auth.login') }}</va-button>
+  </form>-->
+  <div class="g-signin2 d-flex justify--center" data-onsuccess="oauthSignIn">
     </div>
-  </form>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'login',
-  data () {
-    return {
-      email: '',
-      password: '',
-      keepLoggedIn: false,
-      emailErrors: [],
-      passwordErrors: [],
+  // name: 'login',
+  // data () {
+  //   return {
+  //     email: '',
+  //     password: '',
+  //     emailErrors: [],
+  //     passwordErrors: [],
+  //   }
+  // },
+  // computed: {
+  //   formReady () {
+  //     return !this.emailErrors.length && !this.passwordErrors.length
+  //   },
+  // },
+  methods: {
+    init() {
+      gapi.load('auth2', function() {
+      /* Ready. Make a call to gapi.auth2.init or some other API */
+      });
+    },
+    oauthSignIn() {
+        // this.emailErrors = this.email ? [] : ['Email is required']
+        // this.passwordErrors = this.password ? [] : ['Password is required']
+        // if (!this.formReady) {
+        //   return
+        // }
+      gapi.load('auth2', function() {
+        gapi.auth2.authorize({
+        client_id: '353185858988-h75k9v94ume7fu82mt952jo9l876n3n2.apps.googleusercontent.com',
+        scope: 'email profile openid',
+        fetch_basic_profile: false,
+        response_type: 'id_token permission',
+        hosted_domain: 'goa.bits-pilani.ac.in'
+        }, function(response) {
+            if (response.error) {
+              // An error happened!
+              return;
+            }
+          // The user authorized the application for the scopes requested.
+          var accessToken = response.access_token;
+          var idToken = response.id_token;
+          // You can also now use gapi.client to perform authenticated requests.
+        });
+      });
     }
   },
-  computed: {
-    formReady () {
-      return !this.emailErrors.length && !this.passwordErrors.length
-    },
-  },
-  methods: {
-    onsubmit () {
-      this.emailErrors = this.email ? [] : ['Email is required']
-      this.passwordErrors = this.password ? [] : ['Password is required']
-      if (!this.formReady) {
-        return
-      }
-      this.$router.push({ name: 'dashboard' })
-    },
-  },
+  mounted() {
+    var AuthStr
+    axios.post('http://csa.devsoc.club/api/v1/google/student/signin',  { headers: {idToken: this.accessToken} })
+    .then(response => AuthStr)
+  }
 }
 </script>
 
--- dashboard/src/components/dashboard/Dashboard.vue
+++ dashboard/src/components/dashboard/Dashboard.vue
@@ -1,5 +1,159 @@
 <template>
+<div>
   <div class="dashboard">
+
+    <div class="timelines">
+    <div class="row">
+      <div class="flex xs12">
+        <va-card
+          no-padding-h
+          style="overflow-x: auto;"
+          class="timelines__horizontal-long"
+          :title="('Latest Updates')"
+        >
+          <va-timeline align-top style="min-width: 600px;" class="timelines__horizontal-long__timeline">
+            <va-timeline-item active>
+              <template slot="before">
+                <div
+                  class="title text--center"
+                  :style="{color: $themes.primary}"
+                >
+                  {{ start1 }} Sample date
+                </div>
+              </template>
+              <va-card
+                slot="after"
+                stripe="warning"
+                class="mb-0"
+              >
+                <template slot="title">{{ start1 }}</template>
+                <va-accordion>
+                  <va-collapse>
+                    <span slot="header"> {{ name1 }} Sample name </span>
+                      <div slot="body">
+                        <div>
+                          {{ summary1 }} Sample content
+                        </div>
+                      </div>
+                  </va-collapse>
+                </va-accordion>
+              </va-card>
+            </va-timeline-item>
+            <va-timeline-item active>
+              <template slot="before">
+                <div
+                  class="title text--center"
+                  :style="{color: $themes.primary}"
+                >
+                  {{ start2 }}
+                </div>
+              </template>
+              <va-card
+                slot="after"
+                stripe="info"
+                class="mb-0"
+              >
+                <template slot="title">{{ start2 }}</template>
+                <va-accordion>
+                  <va-collapse>
+                    <span slot="header"> {{ name2 }}  </span>
+                      <div slot="body">
+                        <div>
+                          {{ summary2 }}
+                        </div>
+                      </div>
+                  </va-collapse>
+                </va-accordion>
+              </va-card>
+            </va-timeline-item>
+            <va-timeline-item active>
+              <template slot="before">
+                <div
+                  class="title text--center"
+                  :style="{color: $themes.primary}"
+                >
+                  {{ start3 }}
+                </div>
+              </template>
+              <va-card
+                slot="after"
+                stripe="info"
+                class="mb-0"
+              >
+                <template slot="title">{{ start3 }}</template>
+                <va-accordion>
+                  <va-collapse>
+                    <span slot="header"> {{ name3 }}  </span>
+                      <div slot="body">
+                        <div>
+                          {{ summary3 }}
+                        </div>
+                      </div>
+                  </va-collapse>
+                </va-accordion>
+              </va-card>
+            </va-timeline-item>
+            <va-timeline-item active>
+              <template slot="before">
+                <div
+                  class="title text--center"
+                  :style="{color: $themes.primary}"
+                >
+                  {{ start4 }}
+                </div>
+              </template>
+              <va-card
+                slot="after"
+                stripe="info"
+                class="mb-0"
+              >
+                <template slot="title">{{ start4 }}</template>
+                <va-accordion>
+                  <va-collapse>
+                    <span slot="header"> {{ name4 }}  </span>
+                      <div slot="body">
+                        <div>
+                          {{ summary4 }}
+                        </div>
+                      </div>
+                  </va-collapse>
+                </va-accordion>
+              </va-card>
+            </va-timeline-item>
+            <va-timeline-item active>
+              <template slot="before">
+                <div
+                  class="title text--center"
+                  :style="{color: $themes.primary}"
+                >
+                  {{ start5 }}
+                </div>
+              </template>
+              <va-card
+                slot="after"
+                stripe="info"
+                class="mb-0"
+              >
+              <template slot="title">{{ start5 }}</template>
+                <va-accordion>
+                  <va-collapse>
+                    <span slot="header"> {{ name5 }}  </span>
+                      <div slot="body">
+                        <div>
+                          {{ summary5 }}
+                        </div>
+                      </div>
+                  </va-collapse>
+                </va-accordion>
+              </va-card>
+            </va-timeline-item>
+          </va-timeline>
+        </va-card>
+      </div>
+    </div>
+    </div>
+
+
     <dashboard-charts />
     <dashboard-info-block />
     <div class="row">
@@ -15,13 +169,15 @@
         <dashboard-map ref="dashboardMap"/>
       </div>
     </div>
-  </div>
+  </div>       
+</div>
 </template>
 
 <script>
 import DashboardInfoBlock from './DashboardInfoBlock'
 import DashboardTable from './DashboardTable'
 import DashboardTabs from './DashboardTabs'
+import axios from 'axios'
 
 export default {
   name: 'dashboard',
@@ -36,6 +192,42 @@ export default {
       this.$refs.dashboardMap.addAddress({ city: city.text, country })
     },
   },
+
+name: 'timelines',
+  data () {
+    return {
+      // dateFirst: '',
+      // titleFirst: 'Make design',
+      // titleSecond: 'Develop an app',
+      // titleThird: 'Submit an app',
+      // horizontalSimpleContentFirst: 'Pre-sail rate: 50%',
+      // dateSecond: 'May 22 10:00',
+      // horizontalSimpleContentSecond: 'Pre-sail rate: 40%',
+      // dateThird: 'July 19 17:45',
+      // horizontalSimpleContentThird: 'Pre-sail rate: 20%',
+      // contentFirst: 'The unique stripes of zebras make them one of the animals most familiar to people.',
+      // contentSecond: 'They occur in a variety of habitats, such as grasslands, savannas, woodlands, thorny scrublands.',
+      // contentThird: 'However, various anthropogenic factors have had a severe impact on zebra populations',
+    start1: null, summary1: null, name1: null,
+    start2: null, summary2: null, name2: null,
+    start3: null, summary3: null, name3: null,
+    start4: null, summary4: null, name4: null,
+    start5: null, summary5: null, name5: null
+    }
+  },
+  mounted () {
+        axios.get('https://csa.devsoc.club/api/v1/en/student/show', { headers: {token: AuthStr} })
+         .then(response => {
+         (this.start1 = response.en.reverse()[0].start), (this.summary1 = response.en.reverse()[0].summary),
+         (this.start2 = response.en.reverse()[1].start), (this.summary2 = response.en.reverse()[1].summary),
+         (this.start3 = response.en.reverse()[2].start), (this.summary3 = response.en.reverse()[2].summary),
+         (this.start4 = response.en.reverse()[3].start), (this.summary4 = response.en.reverse()[3].summary),
+         (this.start5 = response.en.reverse()[4].start), (this.summary5 = response.en.reverse()[4].summary),
+         (this.name1 = response.en.reverse()[0].name),(this.name2 = response.en.reverse()[1].name),
+         (this.name3 = response.en.reverse()[2].name),(this.name4 = response.en.reverse()[3].name),
+         (this.name5 = response.en.reverse()[4].name)
+         })
+  }
 }
 </script>
 
