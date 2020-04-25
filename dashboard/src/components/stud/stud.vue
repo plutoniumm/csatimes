5091bf8eef9be5d47d7e07d1572c0dbbcb2d3e9b
Manav Seksaria <ssmaanv@icloud.com>
2020-04-25 23:39:33 +0530

    Demo Revoval

--- dashboard/src/components/stud/stud.vue
+++ dashboard/src/components/stud/stud.vue
@@ -6,9 +6,15 @@
           :title="$t('Campus Life')"
           color="info"
         >
-          <router-link :to="{ path: '../hall'}" append><va-button color="danger"> Hall of Fame</va-button></router-link>
-          <router-link :to="{ path: '../hall'}" append><va-button color="warning"> BITS Goa Startups</va-button></router-link>
-          <router-link :to="{ path: '../hall'}" append><va-button color="success"> Humanities Electives</va-button></router-link>
+          <router-link :to="{ path: '../hall'}" append exact>
+            <va-button color="danger"> Hall of Fame</va-button>
+          </router-link>
+          <router-link :to="{ path: '../hall'}" append exact>
+            <va-button color="warning"> BITS Goa Startups</va-button>
+          </router-link>
+          <router-link :to="{ path: '../hall'}" append exact>
+            <va-button color="success"> Humanities Electives</va-button>
+          </router-link>
         </va-card>
       </div>
     </div>
