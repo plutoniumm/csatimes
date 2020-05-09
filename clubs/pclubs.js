e6830f1b8224e0d844ce861f227641c04f930335
Manav Seksaria <ssmaanv@icloud.com>
2020-05-09 16:58:04 +0530

    Rollback

---
+++ clubs/pclubs.js
@@ -0,0 +1,241 @@
+var techclubs = [
+      {
+    Name: "Drama Club",
+    Logo:"./pclubs/drama/logo.jpg",
+    Founded:"2006",
+    Description:"Joining drama as a student consultant is a great way to boost your CV with relevant work- and consulting experience, while expanding your professional network. Our consultants get a hands on experience working with clients on a wide range of topics and issues employing strategies to achieve the the firms objective. Moreover, you will expand your social network by meeting like-minded students from different countries, study backgrounds and universities.",
+    PORs: [
+    {
+    Photo:"./pclubs/drama/prez.jpg",
+    Title: "Coordinator",
+    Name: "Shashank Kataria",
+    Phone: "+91 95407 03800",
+    Email: "shashankkataria@hotmail.com"
+    },
+    {
+    Photo:"./pclubs/drama/vprez.jpg",
+    Title: "Sub Coordinator",
+    Name: "Krishna Dutta",
+    Phone: "+91 81691 48620",
+    Email: "mpatra@drama.org"
+    }]},
+{Name: "Fash Club",
+Logo:"./pclubs/fash/logo.jpg",
+    Founded:"Unknown",
+    Description:"We are a bunch of fashion enthusiasts who wish to express artistic freedom in the fashion world. The Fashion Club, Bits Goa provides a welcoming and creative environment to the fashion enthusiasts where people can bring out their creativity and talent in the field of fashion designing and modelling. We as a club stand for and want to spread the positive message of being undefined - not letting anything bind you down and making you believe that who you are is not merely what you wear.",
+PORs: [
+    {
+    Photo:"./pclubs/fash/Prez.jpg",
+    Title: "Chief Coordinator",
+    Name: "Pranjal Choubey",
+    Phone: "+91 91317 86237",
+    Email: "pranjalch99@gmail.com"
+    },
+    {
+    Photo:"./pclubs/fash/vprez.jpg",
+    Title: "Executive Coordinator",
+    Name: "Shravan Chaudhari",
+    Phone: "+91 91300 91125",
+    Email: "shravan25.chaudhari2@gmail.com"
+    },
+    {
+    Photo:"./pclubs/fash/trez.jpg",
+    Title: "Assets Head",
+    Name: "Nischay Solanki",
+    Phone: "+91 89202 53178",
+    Email: "nishchaysolanki97@gmail.com"
+    },
+    {
+    Photo:"./pclubs/fash/sec.jpg",
+    Title: "Waves Coordinator",
+    Name: "Anand Prabhakar",
+    Phone: "+91 90647 81470",
+    Email: "f20170289@goa.bits-pilani.ac.in"
+    }]},
+    {Name: "Film Making Club",
+    Logo:"./pclubs/fmac/logo.png",
+        Founded:"Unknown",
+        Description:"A group of misfits, and movie enthusiasts ready to entertain people with their story telling skills. Just two years old and growing fast, they intend to bring new ideas, in a fresh packaging, through the medium of cinema. This knack to entertain you a bit more, makes these misfits fit in a little more! With different paths, interests and skills, they aim to cater to your very unique taste in movies, and bring to you, Your Personal Dose of ENTERTAINMENT!!!",
+    PORs: [
+        {
+        Photo:"./pclubs/fmac/Prez.png",
+        Title: "Chief Coordinator",
+        Name: "Sai Bharadwaj",
+        Phone: "+91 70137 55099",
+        Email: "sarangulasaibharadwaj@gmail.com"
+        },
+        {
+        Photo:"./pclubs/fmac/Vprez.jpeg",
+        Title: "Coordinator",
+        Name: "Sumir Kaustubh",
+        Phone: "+91 76599 84944",
+        Email: "sumirkaustubh.nellutla@gmail.com"
+        },
+        {
+        Photo:"./pclubs/fmac/sec.png",
+        Title: "Technical Head",
+        Name: "Akul Badami",
+        Phone: "+91 77606 66905",
+        Email: "akulbadami4@gmail.com"
+        }]},
+        {
+      Name: "Kala",
+      Logo:"./pclubs/kala/logo.jpg",
+      Founded:"Unknown",
+      Description:"",
+      PORs: [
+      {
+      Photo:"./pclubs/kala/prez.png",
+      Title: "Coordinator",
+      Name: "Ananya Patnaik",
+      Phone: "+91 80190 01773",
+      Email: "f20171935@goa.bits-pilani.ac.in"
+      },
+      {
+      Photo:"./pclubs/kala/vprez.jpg",
+      Title: "Coordinator",
+      Name: "Rishikesh Vanarse",
+      Phone: "+91 77980 14893",
+      Email: "f20170525@goa.bits-pilani.ac.in"
+      }]},
+    {
+      Name: "Mime Club",
+      Logo:"./pclubs/mime/logo.jpg",
+      Founded:"2006",
+      Description:"A performing club with performances ranging from the colourful UV/LED acts to the captivating Shadow acts, and of course, our oldest format - the rib tickling Skyme.",
+      PORs: [
+      {
+      Photo:"./pclubs/mime/prez.jpg",
+      Title: "Chief Coordinator",
+      Name: "Tejaswini Jupudi",
+      Phone: "+91 99518 22407",
+      Email: "99tejaswini@gmail.com"
+      },
+      {
+      Photo:"./pclubs/mime/vprez.jpg",
+      Title: "Sub Coordinator",
+      Name: "Unknown",
+      Phone: "+91 94964 79572",
+      Email: "albriteben99@gmail.com"
+      }]},
+      {Name: "Music Society",
+      Logo:"./pclubs/musoc/logo.jpg",
+      Description:"The Music Society, BITS Goa is the official music club of the campus. We are a bunch of musicians interested in a wide variety of genres constantly looking to expand our horizons and create new music. In general, we jam, organize music related events, perform and most importantly have fun",
+      Founded:"Unknown",
+PORs: [
+      {
+      Photo:"./pclubs/musoc/Prez.jpg",
+      Title: "Coordinator",
+      Name: "Shruti Iyer",
+      Phone: "",
+      Email: ""
+      },
+      {
+      Photo:"./pclubs/musoc/Vprez.jpg",
+      Title: "Sub Coordinator",
+      Name: "Bharat Rajagopalan",
+      Phone: "+91 97630 26112",
+      Email: "f20170486@goa.bits-pilani.ac.in"
+      }]},
+      {Name: "Silver Screen",
+      Logo:"./pclubs/screen/logo.png",
+      Founded:"Unknown",
+      Description:"",
+PORs: [
+      {
+      Photo:"./pclubs/screen/Prez.jpg",
+      Title: "Coordinator",
+      Name: "Nihal Pingali",
+      Phone: "+91 91588 72789",
+      Email: "f20170099@goa.bits-pilani.ac.in"
+      },
+      {
+      Photo:"./pclubs/screen/Vprez.jpg",
+      Title: "Sub Coordinator",
+      Name: "Richi Dubey",
+      Phone: "+91 80088 21939 ",
+      Email: "f20171904@goa.bits-pilani.ac.in"
+      }]},
+    //   {
+    //   Name: "Abhivyakti",
+    //   Logo:"./pclubs/abvk/logo.png",
+    //   Founded:"2016",
+    //   Description:"Abhivyakti, is a 'Hindi' word, which means expression. In simplest terms, we're a bunch of people who love to express ourselves through a little bit of drama, a tinge of music, and LOTS and LOTS of POETRY. Poetry, for us, is not an expression of the party line. It's that time of night, lying in bed, where we think what we really think, and make the private world public; that's what a poet does.",
+    //   PORs: [
+    //   {
+    //   Photo:"./pclubs/abvk/Prez.jpg",
+    //   Title: "Coordinator",
+    //   Name:"Tanmay Mathur",
+    //   Phone:"+91 98348 70354",
+    //   Email:"tanmaymathur2000@gmail.com"
+    //   },
+    //   {
+    //   Photo:"./pclubs/abvk/Vprez.jpg",
+    //   Title: "Events Head",
+    //   Name:"Vanshika Kapoor",
+    //   Phone:"+91 98839 24642",
+    //   Email:"vanshikakapoor379@gmail.com"
+    //   },
+    //   {
+    //   Photo:"./pclubs/abvk/Sec.jpg",
+    //   Title: "Content Head",
+    //   Name:"Pranjal Srivastava",
+    //   Phone:"+91 63870 70443",
+    //   Email:"f20170910@goa.bits-pilani.com"
+    //   },
+    //   {
+    //   Photo:"./pclubs/abvk/Trez.png",
+    //   Title: "Litrary Head",
+    //   Name:"Mrigank Jain",
+    //   Phone:"+91 78881 46052",
+    //   Email:"f20170458@goa.bits-pilani.ac.in"
+    //   }]},
+      {
+      Name: "Synchronoise",
+      Logo:"./pclubs/sync/logo.png",
+      Founded:"2019",
+      Description:"",
+      PORs: [
+      {
+      Photo:"./pclubs/sync/Prez.png",
+      Title: "Coordinator",
+      Name: "Aditya Chobe",
+      Phone: "+91",
+      Email: ""
+      },
+      {
+      Photo:"./pclubs/sync/Vprez.jpg",
+      Title: "Sub Coordinator",
+      Name: "Navya",
+      Phone: "+91",
+      Email: ""
+      }]},
+      {
+        Name: "Literary and Debating Club",
+        Logo:"./pclubs/ldc/logo.jpg",
+        Founded:"2019",
+        Description:"We’re a bunch of avid readers, funny writers, debaters, loony comedians and more. We value the ability to take a joke higher than the ability to crack one. We find humour in the strangest little things. As far as mixing work and play is concerned, we have thin lines between the two.",
+        PORs: [
+        {
+        Photo:"./pclubs/ldc/Prez.png",
+        Title: "Coordinator",
+        Name: "Atharv Kirtikar",
+        Phone: "+91 89752 97801",
+        Email: "f20180107@goa.bits-pilani.ac.in"
+        },
+      {
+      Photo:"./pclubs/ldc/Prez.png",
+      Title: "Literary Head",
+      Name: "Atharv Phadnis ",
+      Phone: "+91 7350 95005",
+      Email: "asphadnis1@gmail.com"
+      },
+      {
+      Photo:"./pclubs/ldc/Prez.png",
+      Title: "Coordinator",
+      Name: "Aditya Sunkara",
+      Phone: "+91 97915 13743",
+      Email: "f20180316@goa.bits-pilani.ac.in"
+      }]}
+
+];
