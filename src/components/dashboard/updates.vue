345d0dc1a90bbee8b8c34ad6045b15f316818483
Aviral09 <aviralgoel2001@gmail.com>
2020-06-03 20:55:33 +0530

    Dashboard fix

--- src/components/dashboard/updates.vue
+++ src/components/dashboard/updates.vue
@@ -36,17 +36,20 @@
               </va-timeline>
             </va-card> -->
 
-      <va-card slot="after" stripe="danger" class="mb-0">
-        <h2>Aseem Juneja</h2>
-        <div>
-          <div><span style="color: #a7a7a7;">Yesterday, 10:00 PM</span>
-            <hr>
-            <strong>Title of the post below</strong> <br>
-            This is the post detail. This is the post detail. This is the post detail. This is the post detail. This is the post detail. This is the post detail. This is the post detail. This is the post detail
+      <template v-for="(notice, i) in start1">
+        <va-card slot="after" stripe="danger" class="mb-0" :key="'item' + notice.name">
+          <h2>{{ notice.writer }}</h2>
+          <div>
+            <div><span style="color: #a7a7a7;">{{ notice.updated_at.substring(0,10) }}</span>
+              <hr>
+              <strong>{{ notice.name }}</strong> <br>
+              {{ notice.summary }}
+            </div>
+            <span style="color: #a7a7a7;">Brought to you by CSA</span>
           </div>
-          <span style="color: #a7a7a7;">Brought to you by CSA</span>
-        </div>
-      </va-card>
+        </va-card>
+        <br>
+      </template>
 
     </div>
   </div>
@@ -67,14 +70,6 @@ export default {
   data () {
     return {
       start1: null,
-      pr: null,
-      nick: null,
-      withDescription: '',
-      image: [],
-      simpleOptions: [],
-      simpleSelectModel: '',
-      checkbox: false,
-      value: 1,
     }
   },
   mounted () {
@@ -83,14 +78,7 @@ export default {
         headers: { token: this.$AuthStr },
       })
       .then(response => {
-        this.start1 = response.data.en
-      })
-    axios
-      .get('https://csa.devsoc.club/api/v1/genpr/student/getAllsubEvents', {
-        headers: { token: this.$AuthStr },
-      })
-      .then(response => {
-        this.pr = response.data.subEvents
+        this.start1 = response.data.en.reverse()
       })
   },
 }
